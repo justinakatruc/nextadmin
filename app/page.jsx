2c326f6727d1ad86b5dfaa6f0421c9b52dde4cf3
Truc Minh Nguyen <minhtruc103@gmail.com>
2025-05-27 19:40:03 +0700

    Updated Homepage (SSH key test)

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import { redirect } from 'next/navigation';
 
 const Homepage = () => {
   redirect('/login');
-  return null; // This component will never render because of the redirect
 }
 
 export default Homepage
